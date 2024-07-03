61b1d38bd1237b619042e6763dcf7aa620cf3916
tubarao19 <miguelmonsalvo19@gmail.com>
2024-07-02 22:25:30 -0500

    models

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
+from models.item_model import Item
 
 app = FastAPI()
+
 
 @app.get('/')
 def read_root():
@@ -14,3 +16,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get('/calculadora')
 def calcular(op: float, op2: float):
     return {'suma': op + op2}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: id, item: Item):
+    return {'item_name':item.name, 'item_id':item_id}
